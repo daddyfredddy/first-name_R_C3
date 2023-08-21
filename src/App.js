7c65fed85250c05e87e778d845ec7e6184e48758
Fredrick Codero <fredrickcodero@gmail.com>
2023-08-21 21:33:23 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <h1>'Hello world'</h1>
-    <p>I am Fredrick Codero, this is my first React application</p>
+    <div className="App">
+      <h1>'Hello world'</h1>
+      <p>I am Fredrick Codero, this is my first React application</p>
+    </div>
+    
   );
 }
 
