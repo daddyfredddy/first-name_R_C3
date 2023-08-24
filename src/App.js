389d0835495fb8697c3b31b0c2ea00f610ff8475
Fredrick Codero <fredrickcodero@gmail.com>
2023-08-24 23:49:04 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ function App() {
   return (
     <div className="App">
       <h1>'Hello world'</h1>
-      <p>I am Fredrick Codero, this is my first React application</p>
+      <h2>Full name: Fredrick Codero</h2>
+      <h2>Educational Background: I have studied Electrical and electronic Engineering.</h2>
+      <h2>Professional Background: Worked as broadcast Engineer. </h2>
+      <h2>Skills: Outside broadcast management.</h2>
     </div>
     
   );
