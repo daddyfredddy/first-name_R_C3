7625ecf47758944ef938068d3f74384fcd07d8d8
Fredrick Codero <fredrickcodero@gmail.com>
2023-08-26 17:59:09 +0300

    added label props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import Button from "./components/Button";
 
 function App() {
   return (
     <div className="App">
       <h1>'Hello world'</h1>
       <h2>Full name: Fredrick Codero</h2>
-      <h2>Educational Background: I have studied Electrical and electronic Engineering.</h2>
+      <h2>
+        Educational Background: I have studied Electrical and electronics
+        Engineering.
+      </h2>
       <h2>Professional Background: Worked as broadcast Engineer. </h2>
       <h2>Skills: Outside broadcast management.</h2>
+      <Button/>
     </div>
-    
   );
 }
 
---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+const Button = ({ Login, Signin }) => {
+  return (
+    <div>
+      <button id="click">LOG IN</button>
+      <button id="click">SIGN IN</button>
+    </div>
+  );
+};
+
+export default Button;
